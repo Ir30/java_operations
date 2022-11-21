450ab01e3f15fa0d1cf3a089ff801c41a9fa7658
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-21 12:27:39 +0530

    progressing..

--- javaOperations.java
+++ javaOperations.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 class javaOperations{
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the choice: ");
+        System.out.println("------------------------");
+        System.out.println();
+        System.out.println("1.largest of three numbers");
+        System.out.println("2.smallest of three numbers");
+        System.out.println("3.check prime or not");
+        System.out.println("4.check even or not");
+        System.out.println("5.Reverse of a number");
+        System.out.println("6.Exit");
+        System.out.println();
+        System.out.println("------------------------");
+        System.out.println();
+
+        int choice = sc.nextInt();
+
+
+
     }
 }
